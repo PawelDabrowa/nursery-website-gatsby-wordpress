ffb2982e209ab6fdcdbb90a31ac5de583c879b23
Pawel Dabrowa <fox@Pawels-iMac.local>
2020-07-18 15:34:38 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Przedszkole Karoliny`,
+    description: `Strona przedszkola Karoliny`,
+    author: `Pawel Dabrowa`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,38 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: 'Lato',
+            variants: ['200', '400', '500', '600', '700'],
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        excludedRoutes: [
+          '/wp/v2/users/**',
+          '/wp/v2/settings*',
+          '/wp/v2/themes*',
+        ],
+        baseUrl: 'przedszkole.test',
+        protocol: 'http',
+        hostingWPCOM: false,
+        useACF: true,
+        searchAndReplaceContentUrls: {
+          sourceUrl: 'http://przedszkole.test/',
+          replacementUrl: '',
+        },
+      },
+    },
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,28 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import Header from "../components/header"
 import SEO from "../components/seo"
 
+import "./styles.scss"
+
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <Header/>
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <h2 className="title is-2">Level 2 heading</h2>
+          <p className="content">Cool content. Using Bulma!</p>
+        </div>
+        <div className="colum">
+          <h2 className="title is-2">Level 2 heading</h2>
+          <p className="content">This column is cool too!</p>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
